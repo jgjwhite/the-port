7fb6e86125b0b4bdf6353fc7113114e890efc6d7
Saverio Nardi <sav.nardi@gmail.com>
2016-09-11 23:52:52 -0400

    Get banner working properly with animations and parallax

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-$('.parallax-window').parallax({
-    naturalWidth: 930,
-    naturalHeight: 400
+window.addEventListener('scroll', (event) => {
+    var topDistance = window.pageYOffset;
+    var layers = document.querySelectorAll("[data-type='parallax']");
+
+    layers.forEach(layer => {
+        var depth = layer.getAttribute('data-depth');
+        var movement = -(topDistance * depth);
+        var translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+        layer.style['-webkit-transform'] = translate3d;
+        layer.style['-moz-transform'] = translate3d;
+        layer.style['-ms-transform'] = translate3d;
+        layer.style['-o-transform'] = translate3d;
+        layer.style.transform = translate3d;
+    });
+
 });
